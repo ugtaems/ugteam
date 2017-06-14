68f7e0db974e79e532b0a363e0095cf999ebc18b
ugtaems <ug.taems@yahoo.com>
2017-06-14 11:58:53 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNTER=1
 while(true) do
-./parsol.sh
+./ugtaems.sh
 curl "https://api.telegram.org/bot[ToKeN]/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
