b2c9776642088fd69978a499baff343c663aa5de
ugtaems <ug.taems@yahoo.com>
2017-08-05 03:08:55 -0700

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,6 +1,8 @@
 do local _ = {
   admins = {},
-  disabled_channels = {},
+  disabled_channels = {
+    ["-1001149925609"] = true
+  },
   enabled_plugins = {
     "banhammer",
     "groupmanager",
@@ -16,14 +18,21 @@ do local _ = {
     "time1",
     "write",
     "addkick",
+    "SolvedOffline",
+    "clean",
+    "warn",
+    "muteall",
+    "tamde",
+    "CodeGift",
+    "muteall"
   },
-  info_text = "》راهنمای تمدید اعتبار رباتv4.1\n\n🔰 شماره کارت بانک ملی ایران :\n》 6037997303171871 《\n\n➖➖➖➖➖➖➖➖\n\n🔰ایدی مدیریت فروش :\n》 @arta_2000 《\n\n➖➖➖➖➖➖➖➖\n\n🔰ایدی مدیرپشتیبانی  نصب و راه اندازی ربات :\n》@mu12123《\n\n➖➖➖➖➖➖➖➖\n\n🔰نظرات وپیشنهادات خود را باما در میان بزارید:\n》@mu12123《\n》@arta_2000《\n》@kingvan《\n\n➖➖➖➖➖➖➖➖\n\n🔰.ادرس سایت ما:\n》http://ug-b-z-l.ir 《\n\n➖➖➖➖➖➖➖➖\n\n🔰لینک پشتیبانی  ربات های ضد لینکug :\n》 https://t.me/joinchat/ETnLRj-km-o8h3XCuw4u8A 《\n\n➖➖➖➖➖➖➖➖\n\n🔰.تیم پشتیبانی ربات های ضد لینک ugبا ما  امنیت  رو  تجربه کنید .\n",
+  info_text = "》راهنمای تمدید اعتبار رباتv4.1\n\n🔰 شماره کارت بانک ملی ایران :\n》 6037997303171871 《\n\n➖➖➖➖➖➖➖➖\n\n🔰ایدی مدیریت فروش :\n》 @Princessonline 《\n\n➖➖➖➖➖➖➖➖\n\n🔰ایدی مدیرپشتیبانی  نصب و راه اندازی ربات :\n》@mu12123《\n\n➖➖➖➖➖➖➖➖\n\n🔰نظرات وپیشنهادات خود را باما در میان بزارید:\n》@mu12123《\n》@arta_2000《\n》@kingvan《\n\n➖➖➖➖➖➖➖➖\n\n🔰.ادرس سایت ما:\n》http://ug-b-z-l.ir 《\n\n➖➖➖➖➖➖➖➖\n\n🔰لینک پشتیبانی  ربات های ضد لینکug :\n》 https://t.me/joinchat/ETnLRj-km-o8h3XCuw4u8A 《\n\n➖➖➖➖➖➖➖➖\n\n🔰.تیم پشتیبانی ربات های ضد لینک ugبا ما  امنیت  رو  تجربه کنید .\n",
   moderation = {
     data = "./data/moderation.json"
   },
   sudo_users = {
-   378362487,
-   289000262,		
+    378362487,
+    289000262
   }
 }
 return _
